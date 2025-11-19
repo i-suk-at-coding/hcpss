833209c7d33524edf836792a1df0f3e2786b8cb2
i-suk-at-coding <GrantL2024@hotmail.com>
2025-11-18 20:48:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
@@ -8,17 +9,26 @@ const io = socketIO(server);
 
 app.use(express.static('public'));
 
-let chatHistory = []; // store messages
+let chatHistory = [];
+
+// Load history from file if exists
+if (fs.existsSync('history.json')) {
+  chatHistory = JSON.parse(fs.readFileSync('history.json', 'utf8'));
+}
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Send existing history to new user
+  // Send history to new user
   socket.emit('chat history', chatHistory);
 
   socket.on('chat message', (msg) => {
-    chatHistory.push(msg); // save message
-    io.emit('chat message', msg); // broadcast
+    chatHistory.push(msg);
+
+    // Save history to file
+    fs.writeFileSync('history.json', JSON.stringify(chatHistory));
+
+    io.emit('chat message', msg);
   });
 
   socket.on('disconnect', () => {
