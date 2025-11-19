fb10901a9d52d33bfabab25aa47f647c5ddf6959
i-suk-at-coding <GrantL2024@hotmail.com>
2025-11-18 20:35:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// run `node index.js` in the terminal
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -9,11 +8,19 @@ const io = socketIO(server);
 
 app.use(express.static('public'));
 
+let chatHistory = []; // store messages
+
 io.on('connection', (socket) => {
   console.log('A user connected');
+
+  // Send existing history to new user
+  socket.emit('chat history', chatHistory);
+
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    chatHistory.push(msg); // save message
+    io.emit('chat message', msg); // broadcast
   });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
