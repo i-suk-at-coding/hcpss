d74c8aadbb52efab7c2dc9203beadf49ee7bdd13
i-suk-at-coding <GrantL2024@hotmail.com>
2025-11-26 13:43:07 -0500

    Update server.js

--- platformer/server.js
+++ platformer/server.js
@@ -17,8 +17,74 @@ const WORLD = {
   height: 2000,
   spawn: { x: 200, y: 100},   // ✅ NEW: default spawn point
   platforms: [
-    {"x":56,"y":616,"w":798,"h":54,"rotation":-152.02052561151987,"material":"bounce"},
-    {"x":772,"y":803,"w":1194,"h":50,"rotation":0,"material":"ice"}
+    {
+  "platforms": [
+    {
+      "x": 45,
+      "y": 151,
+      "w": 200,
+      "h": 20,
+      "rotation": -152.3116461088466,
+      "material": "ice",
+      "rotationHandle": {
+        "x": 145,
+        "y": 111
+      }
+    },
+    {
+      "x": 188,
+      "y": 211,
+      "w": 200,
+      "h": 20,
+      "rotation": -165.96375653207352,
+      "material": "ice",
+      "rotationHandle": {
+        "x": 288,
+        "y": 171
+      }
+    },
+    {
+      "x": 364,
+      "y": 239,
+      "w": 200,
+      "h": 20,
+      "rotation": -177.54596832547296,
+      "material": "ice",
+      "rotationHandle": {
+        "x": 464,
+        "y": 199
+      }
+    },
+    {
+      "x": 543,
+      "y": 244,
+      "w": 200,
+      "h": 20,
+      "rotation": 0,
+      "material": "ice",
+      "rotationHandle": {
+        "x": 643,
+        "y": 204
+      }
+    },
+    {
+      "x": 696,
+      "y": 245,
+      "w": 896,
+      "h": 19,
+      "rotation": 0,
+      "material": "ice",
+      "rotationHandle": {
+        "x": 1144,
+        "y": 205
+      }
+    }
+  ],
+  "spawn": {
+    "x": 100,
+    "y": 100
+  }
+}
   ]
 };
 
@@ -167,7 +233,7 @@ function stepPlayer(p, dt) {
         grounded=true;
         p.vy=0;
         switch(plat.material){
-          case "ice": p.vx*=2; break;
+          case "ice": p.vx*=1.25; break;
           case "lava": respawnPlayer(p); break;
           case "bounce": p.vy=-800; break;
           case "sticky": p.vx*=0.5; break;
